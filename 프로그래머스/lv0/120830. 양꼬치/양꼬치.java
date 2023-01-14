3d05a6e8d4df5b8fa2b216bbf8813bdfffddcc45
ongguri <97081494+ongguri@users.noreply.github.com>
2023-01-14 14:53:11 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        int sheep = n * 12000;
+        int beverage = (k - (n / 10)) * 2000;
+        
+        return answer = sheep + beverage;
+    }
+}
