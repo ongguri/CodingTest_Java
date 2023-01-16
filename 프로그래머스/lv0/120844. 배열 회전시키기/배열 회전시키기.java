51ba0ecb735c0d4a83b581863f669ea3512c07ee
ongguri <97081494+ongguri@users.noreply.github.com>
2023-01-16 21:20:00 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.01 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.java
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.java
@@ -1,17 +1,19 @@
 class Solution {
     public int[] solution(int[] numbers, String direction) {
         int[] answer = new int[numbers.length];
+        int nextIndex = 0;
         
         if(direction.equals("right")) {
-            for(int i = 1; i <= numbers.length; i++) {
-                answer[i % numbers.length] = numbers[i-1];
-            }
+            nextIndex = 1;
         }
-        else {
-            for(int i = 0; i < numbers.length; i++) {
-	            answer[(i+numbers.length-1) % numbers.length] = numbers[i];
-	        }
+        else { 
+            nextIndex = numbers.length-1;
         }
+        
+        for(int i = 0; i < numbers.length; i++) {
+            answer[(i+nextIndex) % numbers.length] = numbers[i];
+        }
+        
         return answer;
     }
 }
