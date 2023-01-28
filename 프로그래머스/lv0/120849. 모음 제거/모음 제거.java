222a05308e1a323b87ba94cdd23cd6ff1ddeb07d
ongguri <97081494+ongguri@users.noreply.github.com>
2023-01-28 21:31:57 +0900

    [level 0] Title: 모음 제거, Time: 0.15 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -1,9 +1,6 @@
 class Solution {
     public String solution(String my_string) {
-        String[] aeiou = new String[] {"a", "e", "i", "o", "u"};
-        for(String str : aeiou){
-            my_string = my_string.replace(str, "");
-        }
-        return my_string;
+        
+        return my_string.replaceAll("[aeiou]","");
     }
 }
