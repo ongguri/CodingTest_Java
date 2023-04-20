0157c60efd40f7672de90fdec0da9b8742c570cc
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-04-20 22:45:11 +0900

    [Bronze V] Title: A+B, Time: 136 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,11 +1,18 @@
-import java.util.*;
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-        
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
-        
-        System.out.println(num1+num2);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int sum = 0;
+
+        while(st.countTokens() != 0) {
+            sum += Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(sum);
+    }
 }
