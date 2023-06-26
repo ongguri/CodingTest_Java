f510eec34894cec2742e5f3c6b00e70e14c77420
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-06-26 13:18:50 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.47 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -1,14 +1,9 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] numbers) {
-        int answer = -10000*10000;
-        
-        for(int i = 0; i < numbers.length-1; i++) {
-            for(int j = i+1; j < numbers.length; j++) {
-                if (answer < numbers[i]*numbers[j]) {
-                    answer = numbers[i]*numbers[j];
-                }
-            }
-        }
-        return answer;
+        int len = numbers.length;
+        Arrays.sort(numbers);
+        return Math.max(numbers[0]*numbers[1], numbers[len-1]*numbers[len-2]);
     }
 }
