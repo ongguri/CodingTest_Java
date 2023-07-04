ef64d1d0b146999dbd350b1c85b6bee73ba0031b
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-07-04 11:42:47 +0900

    Update k의 개수.java

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.java
@@ -1,7 +1,6 @@
 class Solution {
     public int solution(int i, int j, int k) {
         int answer = 0;
-        String kStr = Integer.toString(k);
             
         for(int num = i; num <= j; num++) {
             String numStr = Integer.toString(num);
