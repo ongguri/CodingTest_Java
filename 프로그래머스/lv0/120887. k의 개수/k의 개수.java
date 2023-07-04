0ae456e599791c0e47428d8be0310e850619c645
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-07-04 11:41:59 +0900

    [level 0] Title: k의 개수, Time: 7.28 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int i, int j, int k) {
+        int answer = 0;
+        String kStr = Integer.toString(k);
+            
+        for(int num = i; num <= j; num++) {
+            String numStr = Integer.toString(num);
+            for(int s = 0; s < numStr.length(); s++) {
+                if(numStr.charAt(s)-'0' == k) {
+                    answer += 1;
+                }
+            }
+        }
+        return answer;
+    }
+}
