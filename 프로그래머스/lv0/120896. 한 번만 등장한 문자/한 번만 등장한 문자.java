3623f6ea4a3d85bc68b95da0a68806a7be72d58e
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-07-06 15:45:16 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 1.95 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        for(int i = 0; i < s.length(); i++) {
+            String str = s.substring(i, i+1);
+            map.put(str, map.getOrDefault(str, 0) + 1);
+        }
+        for(Entry<String, Integer> entrySet : map.entrySet()) {
+            if(entrySet.getValue() > 1) {
+                continue;
+            }
+            answer += entrySet.getKey();
+        }
+        char[] ch = answer.toCharArray();
+        Arrays.sort(ch);
+        answer = new String(ch);
+        
+        return answer;
+    }
+}
