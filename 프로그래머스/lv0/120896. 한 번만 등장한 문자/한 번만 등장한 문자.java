619aef43f872d608eedb91ace9d3079af6d1c810
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-07-06 15:47:16 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 2.79 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -10,12 +10,14 @@ class Solution {
             String str = s.substring(i, i+1);
             map.put(str, map.getOrDefault(str, 0) + 1);
         }
+        
         for(Entry<String, Integer> entrySet : map.entrySet()) {
             if(entrySet.getValue() > 1) {
                 continue;
             }
             answer += entrySet.getKey();
         }
+        
         char[] ch = answer.toCharArray();
         Arrays.sort(ch);
         answer = new String(ch);
