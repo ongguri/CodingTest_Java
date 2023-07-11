7454b6a302a3e9d566fef667fc2f1b1301a77940
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-07-11 20:01:34 +0900

    [level 0] Title: 세균 증식, Time: 0.01 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n, int t) {
+        int answer = n;
+        for(; t > 0; t--) {
+            answer *= 2;
+        }
+        
+        return answer;
+    }
+}
