865390deba0fc0250738ac014f9465f206ba924b
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-07-17 12:09:23 +0900

    [level 0] Title: 옹알이 (1), Time: 0.20 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        for(String str : babbling) {
+            str = str.replace("aya", ".");
+            str = str.replace("ye", ".");
+            str = str.replace("woo", ".");
+            str = str.replace("ma", ".");
+            str = str.replace(".", "");
+            
+            if(str.equals("")) { answer++; }
+        }
+        return answer;
+    }
+}
