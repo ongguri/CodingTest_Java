64c5a9551173a7b4d9fdd70dc7b86d09da1a3f8c
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-07-18 20:16:08 +0900

    [unrated] Title: l로 만들기, Time: 13.57 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
@@ -1,14 +1,5 @@
 class Solution {
     public String solution(String myString) {
-        String answer = "";
-        for(int i = 0; i < myString.length(); i++) {
-            if(myString.charAt(i) < 'l') {
-                answer += "l";
-                continue;
-            }
-            answer += myString.charAt(i);
-            
-        }
-        return answer;
+        return myString.replaceAll("[^l-z]", "l");
     }
 }
