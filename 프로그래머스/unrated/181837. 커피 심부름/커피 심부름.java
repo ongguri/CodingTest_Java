4606d167dc6a2a3660c7ea5df43f413fa6ec1bab
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-07-18 21:05:57 +0900

    [unrated] Title: 커피 심부름, Time: 0.04 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String[] order) {
+        int answer = 0;
+        for(String menu : order) {
+            if(menu.contains("americano") || menu.contains("anything")) {
+                answer += 4500;
+            }
+            else {
+                answer += 5000;
+            }
+        }
+        return answer;
+    }
+}
