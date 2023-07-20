a538ab9d8e9c3ac777d88310b895629a9b7ac253
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-07-20 11:54:03 +0900

    [unrated] Title: 문자열 바꿔서 찾기, Time: 0.06 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -1,16 +1,18 @@
 class Solution {
     public int solution(String myString, String pat) {
-        StringBuilder str = new StringBuilder();
-        for(int i = 0; i < myString.length(); i++) {
-            if(myString.charAt(i) == 'A') {
-                str.append("B");
-            }
-            else if(myString.charAt(i) == 'B') {
-                str.append("A");
-            }
-        }
+        myString = myString.replace("A", "a").replace("B", "A").replace("a", "B");
+        return myString.contains(pat) ? 1 : 0;
+//         StringBuilder str = new StringBuilder();
+//         for(int i = 0; i < myString.length(); i++) {
+//             if(myString.charAt(i) == 'A') {
+//                 str.append("B");
+//             }
+//             else if(myString.charAt(i) == 'B') {
+//                 str.append("A");
+//             }
+//         }
         
-        String result = str.toString();
-        return result.contains(pat) ? 1 : 0;
+//         String result = str.toString();
+//         return result.contains(pat) ? 1 : 0;
     }
 }
