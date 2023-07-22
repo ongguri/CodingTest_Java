21cd905aeb6b096a38eecb42049f84fc5dd74ae3
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-07-22 19:45:27 +0900

    [unrated] Title: 0 떼기, Time: 0.03 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -1,14 +1,16 @@
 class Solution {
     public String solution(String n_str) {
         // return Integer.parseInt(n_str) + "";
-        String answer = "";
-        for(int i = 0; i < n_str.length(); i++) {
-            if(n_str.charAt(i) == '0') {
-                continue;
-            }
-            answer = n_str.substring(i, n_str.length());
-            break;
-        }
-        return answer;
+        return Integer.toString(Integer.parseInt(n_str));
+        
+        // String answer = "";
+        // for(int i = 0; i < n_str.length(); i++) {
+        //     if(n_str.charAt(i) == '0') {
+        //         continue;
+        //     }
+        //     answer = n_str.substring(i, n_str.length());
+        //     break;
+        // }
+        // return answer;
     }
 }
