e5feb2f3abf1c62fc81a9c9c123b2867bb494f33
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-07-22 20:44:18 +0900

    [unrated] Title: 배열 만들기 1, Time: 0.01 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.java
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.java
@@ -2,12 +2,10 @@ class Solution {
     public int[] solution(int n, int k) {
         int[] answer = new int[n / k];
         
-        int idx = 0;
-        for(int i = 1; i <= n; i++) {
-            if(i % k == 0) {
-                answer[idx++] = i; 
-            }
+        for(int i = 1; i <= answer.length; i++) {
+            answer[i-1] = i * k;
         }
+        
         return answer;
     }
 }
