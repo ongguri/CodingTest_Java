ac7189081edb13710c739c0dfa8486b72ab65200
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-07-24 16:46:34 +0900

    [unrated] Title: 코드 처리하기, Time: 0.03 ms, Memory: 70.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -3,11 +3,14 @@ class Solution {
         String answer = "";
         StringBuilder ret = new StringBuilder();
         
-        boolean mode = false;
+        int mode = 0;
         for(int i = 0; i < code.length(); i++) {
-            if(code.charAt(i) == '1') { mode = !mode; }
-            else if(!mode && i % 2 == 0) { ret.append(code.charAt(i)); }
-            else if(mode && i % 2 != 0) { ret.append(code.charAt(i)); }
+            if(code.charAt(i) == '1') {
+                mode = 1 - mode;
+            }
+            else if(i % 2 == mode) {
+                ret.append(code.charAt(i));
+            }
         }
         
         if(ret.toString().equals("")) {
