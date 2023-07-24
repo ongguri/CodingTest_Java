0c291b9323fb813532d46c62718cd37973cdb150
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-07-24 17:27:47 +0900

    [unrated] Title: rny_string, Time: 0.40 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181863. rny＿string/rny＿string.java
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.java
@@ -1,14 +1,14 @@
 class Solution {
     public String solution(String rny_string) {
-        return rny_string.replaceAll("m", "rn");
-//         String[] strArr = rny_string.split("");
-//         for(int i = 0; i < strArr.length; i++) {
-//             if(strArr[i].equals("m")) {
-//                 strArr[i] = "rn";
-//             }
-//         }
+        // return rny_string.replaceAll("m", "rn");
+        String[] strArr = rny_string.split("");
+        for(int i = 0; i < strArr.length; i++) {
+            if(strArr[i].equals("m")) {
+                strArr[i] = "rn";
+            }
+        }
         
-//         String answer = String.join("", strArr);
-//         return answer;
+        String answer = String.join("", strArr);
+        return answer;
     }
 }
