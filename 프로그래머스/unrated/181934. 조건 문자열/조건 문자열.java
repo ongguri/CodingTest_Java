d50eae216eb03856cd29d944e89b9ab3bc16ebab
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-07-25 10:15:40 +0900

    [unrated] Title: 조건 문자열, Time: 0.03 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String ineq, String eq, int n, int m) {
+        boolean answer = false;
+        if(ineq.equals(">")) {
+            answer = n >= m;
+        }
+        else if(ineq.equals("<")) {
+            answer = n <= m;
+        }
+        
+        return answer ? 1 : 0;
+    }
+}
