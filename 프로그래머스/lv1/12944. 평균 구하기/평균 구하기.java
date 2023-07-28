b6cd0bc1dc15abed9b7d596fe3155c80c4a1ce2f
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-07-28 17:16:57 +0900

    [level 1] Title: 평균 구하기, Time: 0.03 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
@@ -2,6 +2,14 @@ import java.util.Arrays;
 
 class Solution {
     public double solution(int[] arr) {
-        return Arrays.stream(arr).average().getAsDouble();
+        // return Arrays.stream(arr).average().getAsDouble();
+        
+        double answer = 0;
+        
+        for(int num : arr) {
+            answer += num;
+        }
+        
+        return answer / arr.length;
     }
 }
