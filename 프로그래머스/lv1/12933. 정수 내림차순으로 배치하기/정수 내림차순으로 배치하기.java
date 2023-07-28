9887d593210cb165a7c50c089d8d5f561a01082c
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-07-28 10:44:42 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.37 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -2,18 +2,28 @@ import java.util.Arrays;
 
 class Solution {
     public long solution(long n) {
-        long answer = 0;
-        long[] nIntArr = new long[Long.toString(n).length()];
+//         long answer = 0;
+//         long[] nIntArr = new long[Long.toString(n).length()];
         
-        for(int i = 0; i < nIntArr.length; i++) {
-            nIntArr[i] = n % 10;
-            n /= 10;
-        }
-        Arrays.sort(nIntArr);
+//         for(int i = 0; i < nIntArr.length; i++) {
+//             nIntArr[i] = n % 10;
+//             n /= 10;
+//         }
+//         Arrays.sort(nIntArr);
         
-        for(int i = 0; i < nIntArr.length; i++) {
-            answer += Math.pow(10, i) * nIntArr[i];
+//         for(int i = 0; i < nIntArr.length; i++) {
+//             answer += Math.pow(10, i) * nIntArr[i];
+//         }
+//         return answer;
+        
+        String[] nStrArr = Long.toString(n).split("");
+        StringBuilder sb = new StringBuilder();
+        
+        Arrays.sort(nStrArr);
+        for(int i = nStrArr.length-1; i >= 0; i--) {
+            sb.append(nStrArr[i]);
         }
-        return answer;
-    }
+        
+        return Long.parseLong(sb.toString());
+    }  // StringBuilder로 해결하면 금방 해결하긴 함.
 }
