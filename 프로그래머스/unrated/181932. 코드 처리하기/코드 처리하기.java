2966d06ccf3f4be0e1f81055704f98774d8b7567
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-07-29 16:38:53 +0900

    [unrated] Title: 코드 처리하기, Time: 0.06 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -2,17 +2,14 @@ class Solution {
     public String solution(String code) {
         String answer = "";
         StringBuilder ret = new StringBuilder();
-        
-        int mode = 0;
+
+        boolean mode = false;
         for(int i = 0; i < code.length(); i++) {
-            if(code.charAt(i) == '1') {
-                mode = 1 - mode;
-            }
-            else if(i % 2 == mode) {
-                ret.append(code.charAt(i));
-            }
+            if(code.charAt(i) == '1') { mode = !mode; }
+            else if(!mode && i % 2 == 0) { ret.append(code.charAt(i)); }
+            else if(mode && i % 2 != 0) { ret.append(code.charAt(i)); }
         }
-        
+
         if(ret.toString().equals("")) {
             answer = "EMPTY";
         }
@@ -20,5 +17,5 @@ class Solution {
             answer = ret.toString();
         }
         return answer;
-    }  // boolean으로 한 것과 int로 한 것 중 뭐가 더 속도가 빠른지 체크
+    }
 }
