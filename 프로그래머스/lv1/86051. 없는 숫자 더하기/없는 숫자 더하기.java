54ead1ae2a527119586d5574bc32aff08cef253f
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-08-03 11:02:05 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,14 +1,22 @@
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 0;
-        int[] numberArr = new int[10];
+//         int answer = 0;
+//         int[] numberArr = new int[10];
+        
+//         for(int num : numbers) {
+//             numberArr[num]++;
+//         }
+        
+//         for(int i = 0; i <= 9; i++) {
+//             if(numberArr[i] == 0) { answer += i; }
+//         }
+        
+//         return answer;
+        
+        int answer = 45;
         
         for(int num : numbers) {
-            numberArr[num]++;
-        }
-        
-        for(int i = 0; i <= 9; i++) {
-            if(numberArr[i] == 0) { answer += i; }
+            answer -= num;
         }
         
         return answer;
