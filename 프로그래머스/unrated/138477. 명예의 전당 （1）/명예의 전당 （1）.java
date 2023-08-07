401fe4b0ef783d5f99cafbbda91f64aab8174eb0
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-08-07 13:27:16 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.42 ms, Memory: 70.4 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,33 +1,25 @@
-import java.util.LinkedList;
+import java.util.*;
 
 class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
-        int day = 0;
-        
-        LinkedList<Integer> rank = new LinkedList<Integer>();
-        rank.add(score[0]);
-        answer[day++] = score[0];
-        
-        for(int i = 1; i < score.length; i++) {
-            boolean isMin = true;
-            for(int j = 0; j < rank.size(); j++) {
-                if(score[i] >= rank.get(j)) {
-                    rank.add(j, score[i]);
-                    isMin = false;
-                    break;
-                }
-                else if(j > k) { break; }
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+
+        int temp = 0;
+
+        for(int i = 0; i < score.length; i++) {
+
+            priorityQueue.add(score[i]);
+            if (priorityQueue.size() > k) {
+                priorityQueue.poll();
             }
-            if(isMin) { rank.add(score[i]); }
-            
-            if(rank.size() < k) {
-                answer[day++] = rank.get(rank.size()-1);
-            }
-            else {
-                answer[day++] = rank.get(k-1);
-            }
+
+            answer[i] = priorityQueue.peek();
         }
+
+
+
         return answer;
     }
 }
