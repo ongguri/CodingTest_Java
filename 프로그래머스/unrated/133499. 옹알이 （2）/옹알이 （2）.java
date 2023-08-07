a61e858294d9034a58051f95f20a014a09237880
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-08-07 21:54:08 +0900

    [unrated] Title: 옹알이 (2), Time: 0.81 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.java
@@ -1,43 +1,23 @@
-import java.util.HashMap;
-
 class Solution {
     public int solution(String[] babbling) {
-        // aya, ye, woo, ma
         int answer = 0;
-        StringBuilder wordSb = new StringBuilder();
-        
-        HashMap<String, Integer> babblingMap = new HashMap<String, Integer>() {{
-            put("aya", 0);
-            put("ye", 0);
-            put("woo", 0);
-            put("ma", 0);
-        }};
         
         for(String babble : babbling) {
-            String prevWord = "";
-            boolean canSay = true;
-            for(int b = 0; b < babble.length(); b++) {
-                wordSb.append(babble.charAt(b));
-                
-                if(babblingMap.get(wordSb.toString()) != null && !prevWord.equals(wordSb.toString())) {
-                    prevWord = wordSb.toString();
-                    wordSb.setLength(0);
-                }
-                else if(wordSb.length() == 3) {
-                    canSay = false;
-                    break;
-                }
-            }
-            if(wordSb.length() != 0) {
-                canSay = false;
-                wordSb.setLength(0);
+            if(babble.contains("ayaaya") || babble.contains("yeye") || babble.contains("woowoo") || babble.contains("mama")) {
+                System.out.println(babble);
+                continue;
             }
             
-            if(canSay) {
-                answer++;
+            babble = babble.replace("aya", ".");
+            babble = babble.replace("ye", ".");
+            babble = babble.replace("woo", ".");
+            babble = babble.replace("ma", ".");
+            babble = babble.replace(".", "");
+            
+            if(babble.length()  == 0) { 
+                answer++; 
             }
         }
-        
         return answer;
     }
 }
