a3456536cff247535d42587ffc9c0e3798d4f78c
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-08-07 09:42:59 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.09 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -2,20 +2,26 @@ class Solution {
     public String solution(int[] food) {
         StringBuilder answer = new StringBuilder();
         
-        for(int i = 1; i < food.length; i++) {
-            int foodCount = food[i] / 2;
-            for(int f = 0; f < foodCount; f++) {
-                answer.append(Integer.toString(i));
+        int idx = 1;
+        boolean reverse = false;
+        while(idx != 0) {
+            int foodCount = food[idx] / 2;
+            
+            if(idx == food.length-1) {
+                answer.append(Integer.toString(idx).repeat(foodCount));
+                answer.append("0");
+                reverse = !reverse;
             }
+            
+            if(!reverse) {
+                answer.append(Integer.toString(idx++).repeat(foodCount));
+            }
+            else if(reverse) {
+                answer.append(Integer.toString(idx--).repeat(foodCount));
+            }
+            
+            
         }
-        
-        answer.reverse();
-        String otherSideFood = answer.toString();
-        answer.reverse();
-        answer.append("0");
-        answer.append(otherSideFood);
-        
-        
         return answer.toString();
     }
 }
