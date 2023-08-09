1d57f73dc196f5c2808f8406cdb9f3c3675e8a82
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-08-09 14:29:23 +0900

    [unrated] Title: 둘만의 암호, Time: 0.37 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
@@ -1,13 +1,21 @@
+import java.util.HashMap;
+
 class Solution {
     public String solution(String s, String skip, int index) {
         StringBuilder answer = new StringBuilder();
+        
+        HashMap<Character, Integer> skipMap = new HashMap<Character, Integer>();
+        for(int i = 0; i < skip.length(); i++) {
+            skipMap.put(skip.charAt(i), 0);
+        }
+        // System.out.println(skipMap);
         
         for(int i = 0; i < s.length(); i++) {
             int idxCount = 0;
             char ch = s.charAt(i);
             while(idxCount < index) {
                 ch = (char)((ch+1 - 'a') % 26 + 'a');
-                if(skip.indexOf(ch) != -1) {
+                if(skipMap.get(ch) != null) {
                     continue;
                 }
                 idxCount++;
