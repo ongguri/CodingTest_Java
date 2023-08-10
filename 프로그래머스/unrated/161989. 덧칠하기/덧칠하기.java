ff70670a1efd4472b8857fade8091ce91eed0764
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-08-10 12:08:15 +0900

    [unrated] Title: 덧칠하기, Time: 2.28 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -1,26 +1,18 @@
-import java.util.LinkedHashMap;
-
 class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 0;
-        int[] roller = new int[section.length];
-        LinkedHashMap<Integer, Integer> sectionMap = new LinkedHashMap<Integer, Integer>();
-        for(int sec : section) {
-            sectionMap.put(sec, 1);
-        }
-        // System.out.println(sectionMap);
         
-        int sectionIdx = 0;
-        for(Integer sec : sectionMap.keySet()) {
-            if(sectionMap.get(sec) == 0) {
-                continue;
-            }
+        int nextPaint = 0;
+        int currentPaint = 0;
+        while(nextPaint < section.length) {
             for(int i = 0; i < m; i++) {
-                if(sectionMap.get(sec+i) != null && sectionMap.get(sec+i) == 1) {
-                    sectionMap.put(sec+i, sectionMap.get(sec+i)-1);
+                if(section[currentPaint]+i == section[nextPaint]) {
+                    nextPaint++;
+                    if(nextPaint >= section.length) { break; }
                 }
             }
             answer++;
+            currentPaint = nextPaint;
         }
         return answer;
     }
