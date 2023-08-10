19825a3ed1c27fdd72200d4dbc09ff36fb24b3a0
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-08-10 14:57:42 +0900

    [unrated] Title: 바탕화면 정리, Time: 0.25 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.java
@@ -5,9 +5,7 @@ class Solution {
         for(int x = 0; x < wallpaper.length; x++) {
             for(int y = 0; y < wallpaper[0].length(); y++) {
                 if(wallpaper[x].charAt(y) == '#') {
-                    if(answer[0] == wallpaper.length) {
-                        answer[0] = x;
-                    }
+                    answer[0] = Math.min(answer[0], x);
                     answer[1] = Math.min(answer[1], y);
                     answer[2] = Math.max(answer[2], x+1);
                     answer[3] = Math.max(answer[3], y+1);
