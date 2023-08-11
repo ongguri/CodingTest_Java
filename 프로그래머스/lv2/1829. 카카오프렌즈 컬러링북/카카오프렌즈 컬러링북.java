9431a89497b4e76e418a2601c581091d83f8be3b
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-08-11 17:18:11 +0900

    [level 2] Title: 카카오프렌즈 컬러링북, Time: 24.15 ms, Memory: 98.1 MB -BaekjoonHub

--- 프로그래머스/lv2/1829. 카카오프렌즈 컬러링북/카카오프렌즈 컬러링북.java
+++ 프로그래머스/lv2/1829. 카카오프렌즈 컬러링북/카카오프렌즈 컬러링북.java
@@ -6,24 +6,18 @@ class Solution {
         int[][] visited = new int[m][n];
         int[] dx = {1, 0, -1, 0};
         int[] dy = {0, 1, 0, -1};
-        HashMap<Integer, Queue<Integer[]>> graphMap = new HashMap<>();
+        Queue<Integer[]> graphQueue = new LinkedList<Integer[]>();
         
-        for(int[] pic : picture) {
-            System.out.println(Arrays.toString(pic));
-        } // 그림 그리는 용도.
-        
-        int area = 1;
         for(int i = 0; i < picture.length; i++) {
             for(int j = 0; j < picture[0].length; j++) {
-                if(picture[i][j] == 0) { continue; }
+                if(picture[i][j] == 0) { continue; }  // 색칠된 부분이 아니라면
                 if(visited[i][j] == 0) { // 방문하지 않았다면
                     int visiteCount = 0;
                     answer[0]++;
-                    graphMap.put(area, new LinkedList<>());
-                    graphMap.get(area).add(new Integer[]{i, j});
+                    graphQueue.add(new Integer[]{i, j});
                     
-                    while(graphMap.get(area).size() != 0) {
-                        Integer[] cur_node = graphMap.get(area).poll();
+                    while(graphQueue.size() != 0) {
+                        Integer[] cur_node = graphQueue.poll();
                         int cx = cur_node[0];
                         int cy = cur_node[1];
                         
@@ -34,13 +28,12 @@ class Solution {
                                picture[nx][ny] != picture[cx][cy] || visited[nx][ny] != 0) {
                                 continue;
                             }
-                            graphMap.get(area).add(new Integer[]{nx, ny});
+                            graphQueue.add(new Integer[]{nx, ny});
                             visited[nx][ny] = 1;
                             visiteCount++;
                         }
                     }
                     answer[1] = Math.max(answer[1], visiteCount);
-                    area++;
                 }
             }
         }
