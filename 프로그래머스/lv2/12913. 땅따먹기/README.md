00a60fdcac6456a4dbce048ef8cd63b2846d5fef
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-08-17 12:28:56 +0900

    [level 2] Title: 땅따먹기, Time: 10.53 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12913. 땅따먹기/README.md
+++ 프로그래머스/lv2/12913. 땅따먹기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 땅따먹기 - 12913 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12913?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12913) 
 
 ### 성능 요약
 
-메모리: 86.2 MB, 시간: 10.28 ms
+메모리: 85.4 MB, 시간: 10.53 ms
 
 ### 구분
 
--- 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.java
@@ -1,28 +1,21 @@
-import java.util.Arrays;
-
 class Solution {
     int solution(int[][] land) {
         int answer = 0;
-        int landLen = land[0].length;
-
-        int x = 1, landIdx = 0;
-        while(x < land.length) {
-            int maxScore = 0;
-            for(int l = 0; l < landLen; l++) {
-                if(l != landIdx) {
-                    maxScore = Math.max(maxScore, land[x-1][l]);
+        
+        for (int x = 1; x < land.length; x++) {
+            for (int y = 0; y < land[0].length; y++) {
+                int maxScore = 0;
+                for (int l = 0; l < land[0].length; l++) {
+                    if (y != l) {
+                        maxScore = Math.max(maxScore, land[x-1][l]);
+                    }
                 }
-            }
-            land[x][landIdx++] += maxScore;
-            
-            if(landIdx == landLen) {
-                landIdx = 0;
-                x++;
+                land[x][y] += maxScore;
             }
         }
         
-        for(int i = 0; i < landLen; i++) {
-            answer = Math.max(answer, land[land.length-1][i]);
+        for(int score : land[land.length-1]) {
+            answer = Math.max(answer, score);
         }
 
         return answer;
