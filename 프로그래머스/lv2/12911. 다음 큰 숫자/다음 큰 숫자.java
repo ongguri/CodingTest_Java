523154cb321da7e2a6b9f38845d870d0adc23074
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-08-17 11:14:27 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.04 ms, Memory: 52.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,12 +1,22 @@
 class Solution {
     public int solution(int n) {
         String nBin = Integer.toBinaryString(n);
-        nBin = nBin.replace("0", "");
-        int len = nBin.length();
+        int len = countOne(nBin);
         
-        while(Integer.toBinaryString(n+1).replace("0", "").length() != len) {
-            n++;
+        while(countOne(Integer.toBinaryString(++n)) != len) {
         }
-        return n+1;
+        
+        return n;
+    }
+    
+    public int countOne(String nBin) {
+        int count = 0;
+        for(int i = 0; i < nBin.length(); i++) {
+            if(nBin.charAt(i) == '1') {
+                count++;
+            }
+        }
+        
+        return count;
     }
 }
