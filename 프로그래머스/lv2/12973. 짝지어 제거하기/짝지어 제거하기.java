839013a9c9a17c992050e92255200f52e4d103d3
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-08-28 11:14:45 +0900

    [level 2] Title: 짝지어 제거하기, Time: 48.56 ms, Memory: 59.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -4,6 +4,8 @@ class Solution {
     public int solution(String s) {
         Stack<Character> stack = new Stack<Character>();
         
+        if(s.length() % 2 != 0) { return 0; }
+        
         for(int i = 0; i < s.length(); i++) {
             if(!stack.isEmpty() && stack.peek() == s.charAt(i)) { stack.pop(); }
             else { stack.push(s.charAt(i)); }
