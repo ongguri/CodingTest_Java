9d6f1663d7da0a222297ed85d0aae198b0c17ff7
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-08-28 09:40:46 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.19 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,16 +1,12 @@
 class Solution {
     public String solution(String s) {
         StringBuilder answer = new StringBuilder();
+        String[] strArr = s.toLowerCase().split("");
+        boolean blank = true;
         
-        for(int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if(i == 0 || s.charAt(i-1) == ' ' && Character.isLowerCase(ch)) {
-                ch = Character.toUpperCase(ch);
-            }
-            else if(s.charAt(i-1) != ' ' && Character.isUpperCase(ch)) {
-                ch = Character.toLowerCase(ch);
-            }
-            answer.append(ch);
+        for(String str : strArr) {
+            answer.append((blank) ? str.toUpperCase() : str);
+            blank = str.equals(" ");
         }
         
         return answer.toString();
