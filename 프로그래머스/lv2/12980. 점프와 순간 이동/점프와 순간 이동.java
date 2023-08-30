2e35f719d50a164d47281d2bfbd33972e14ec81c
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-08-30 11:53:51 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 52.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int solution(int n) {
+        int ans = 0;
+        
+        String bin = Integer.toBinaryString(n);
+        
+        for(int i = 0; i < bin.length(); i++) {
+           if(bin.charAt(i) == '1') { ans++; } 
+        }
+
+        return ans;
+    }
+}
