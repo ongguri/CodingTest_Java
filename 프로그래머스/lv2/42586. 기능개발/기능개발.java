6998cdaa9e5f8193ab33dd2e8a9bc2bbb6bebd31
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-09-05 10:38:27 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.java
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.java
@@ -1,35 +1,31 @@
-import java.util.Queue;
-import java.util.LinkedList;
+import java.util.ArrayList;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        Queue<Integer> completeDay = new LinkedList<Integer>();
-        LinkedList<Integer> result = new LinkedList<Integer>();
+        ArrayList<Integer> deployList = new ArrayList<Integer>();
         
+        int day = 0;
+        int deploy = 1;
         for(int i = 0; i < progresses.length; i++) {
-            int day = 0;
+            int completeDay = 0;
             while(progresses[i] < 100) {
-                day++;
+                completeDay++;
                 progresses[i] += speeds[i];
             }
-            completeDay.add(day);
-        }
-        // System.out.println(completeDay);
-        
-        while(!completeDay.isEmpty()) {
-            int curDay = completeDay.poll();
-            int count = 1;
-            while(!completeDay.isEmpty() && completeDay.peek() <= curDay) {
-                completeDay.poll();
-                count++;
+            
+            if(day < completeDay) {
+                deployList.add(deploy);
+                deploy = 1;
+                day = completeDay;
             }
-            result.add(count);
+            else if(day >= completeDay) { deploy++; }
         }
-        // System.out.println(result);
+        deployList.add(deploy);
+        // System.out.println(deployList);
         
-        int[] answer = new int[result.size()];
-        for(int i = 0; i < answer.length; i++) {
-            answer[i] = result.get(i);
+        int[] answer = new int[deployList.size()-1];
+        for(int i = 1; i <= answer.length; i++) {
+            answer[i-1] = deployList.get(i);
         }
         
         return answer;
