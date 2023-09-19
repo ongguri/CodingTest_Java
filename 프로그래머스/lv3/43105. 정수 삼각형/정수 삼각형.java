59a8c350d991d2dc3e8c755fca17c3e2fcc4037b
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-09-19 17:21:23 +0900

    [level 3] Title: 정수 삼각형, Time: 7.29 ms, Memory: 60.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[][] triangle) {
+        int answer = 0;
+        
+        for(int i = triangle.length-2; i >= 0; i--) {
+            for(int j = 0; j < triangle[i].length; j++) {
+                int max = Math.max(triangle[i+1][j], triangle[i+1][j+1]);
+                triangle[i][j] += max;
+            }
+        }
+        
+        return triangle[0][0];
+    }
+}
